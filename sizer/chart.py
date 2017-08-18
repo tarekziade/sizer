1a29d69b7e75ea31e2737508875b1b763f4bdda0
Tarek Ziadé <tarek@ziade.org>
2017-08-18 11:15:30 +0200

    glances produces csv files that need normalization

--- sizer/chart.py
+++ sizer/chart.py
@@ -1,5 +1,6 @@
 import json
 import os
+import csv
 import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.figure_factory as FF
@@ -9,7 +10,24 @@ import numpy as np
 import pandas as pd
 
 
+def normalize_csv(filename, container_name):
+    prefix = 'docker_' + container_name
+
+    with open(filename) as input:
+        reader = csv.reader(input)
+        rows = 0
+        with open(filename + '.tmp', 'w') as output:
+            writer = csv.writer(output)
+            for i, row in enumerate(reader):
+                if i == 0:
+                    rows = len(row)
+                row = row[:rows]
+                writer.writerow(row)        
+    os.rename(filename + '.tmp', filename)
+
+
 def create_graph(filename, container_name):
+    normalize_csv(filename, container_name)
     prefix = 'docker_' + container_name
     df = pd.read_csv(filename)
     xs = []
@@ -29,4 +47,4 @@ def create_graph(filename, container_name):
 
 
 if __name__ == '__main__':
-    create_graph('/tmp/sizerdata/glances.csv', 'quirky_kalam')
+    create_graph('/tmp/sizerdata/glances.csv', 'thirsty_austin')
