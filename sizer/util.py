edcab7248327268f332bab30eacf0a739aef95c9
Tarek Ziade <tarek@ziade.org>
2017-09-28 16:56:16 +0200

    adding human timestamps

--- sizer/util.py
+++ sizer/util.py
@@ -1,4 +1,13 @@
+import humanize
+import time
+
+
+_STARTED = None
 
 
 def log(msg):
-    print(msg)
+    global _STARTED
+    if _STARTED is None:
+        _STARTED = time.time()
+    age = humanize.naturaltime(time.time() - _STARTED)
+    print("[%s] %s" % (age.capitalize(), msg))
